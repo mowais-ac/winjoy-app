a0bd1d1e4ac2f960dc5fb1ee783729ea119651ec
mahesh-kumar26 <52490092+mahesh-kumar26@users.noreply.github.com>
2021-10-06 18:17:11 +0500

    Last Game Winner Detail Screen is now Dynamic

--- Screens/LastGameWinnerDetail/index.js
+++ Screens/LastGameWinnerDetail/index.js
@@ -1,15 +1,15 @@
 import React, { useState, useRef, useEffect } from "react";
 import {
-    View,
-    StyleSheet,
-    Image,
-    Dimensions,
-    ScrollView,
-    TouchableOpacity,
-    RefreshControl,
-    ActivityIndicator,
-    Text,
-    FlatList
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+  RefreshControl,
+  ActivityIndicator,
+  Text,
+  FlatList,
 } from "react-native";
 import Label from "../../Components/Label";
 const { width, height } = Dimensions.get("window");
@@ -17,185 +17,196 @@ import LinearGradient from "react-native-linear-gradient";
 import HomeBottomList from "../../Components/HomeBottomList";
 import { TriviaAvatar, TriviaCard } from "../../Components";
 import {
-    widthPercentageToDP,
-    heightPercentageToDP,
-    heightConverter,
-    widthConverter,
+  widthPercentageToDP,
+  heightConverter,
+  widthConverter,
 } from "../../Components/Helpers/Responsive";
 import Background from "../../Components/Background";
 import Header from "../../Components/Header";
 import { Avatar } from "react-native-elements";
-import EncryptedStorage from "react-native-encrypted-storage"; 
+import EncryptedStorage from "react-native-encrypted-storage";
 import Config from "react-native-config";
-import axios from 'axios';
-let data = [1, 2, 3]
+import axios from "axios";
+import NotFound from "../../Components/NotFound";
+import ProfilePicture from "../../Components/ProfilePicture";
+import UserInfo from "../../Components/UserInfo";
 const LastGame = ({ props, navigation }) => {
- const [userData, setUserData] = useState([]);
-    useEffect(async () => {
-      GetData()
-    },[]);
-    const GetData = async () => {
-        const Token = await EncryptedStorage.getItem("Token");
-        const requestOptions = {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Accept: "application/json",
-            Authorization: `Bearer ${Token}`,
-          },
-        }; 
-        // alert(13123);
-        
-        await axios.get(`${Config.API_URL}/livegameshow/user/game/history`, requestOptions).then(response => {
-          let res = response;
-          setUserData(res?.data)
-        });
-    
-    }
+  const [userData, setUserData] = useState([]);
+  const [userInfo,setUserInfo] = useState();
+  useEffect(async () => {
+    const userInfo = JSON.parse(await EncryptedStorage.getItem("User"));
+    setUserInfo(userInfo);
+    GetData();
+  }, []);
+  const GetData = async () => {
+    const Token = await EncryptedStorage.getItem("Token");
+    const requestOptions = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Accept: "application/json",
+        Authorization: `Bearer ${Token}`,
+      },
+    };
+    // alert(13123);
 
-    return (
-   
-        <>
-            <Background height={1} />
-            <View style={{ height: 20 }} />
-            <Header back={true} />
-            <View style={styles.aView}>
-                <View style={styles.avatarView}>
-                    <Avatar
-                        rounded
-                        size={130}
+    await axios
+      .get(`${Config.API_URL}/livegameshow/user/game/history`, requestOptions)
+      .then((response) => {
+        let res = response;
+        if (res.data.message === "No Data Found.") {
+          setUserData([]);
+        } else {
+          setUserData(res?.data);
+        }
+      });
+  };
 
-                        // title="MD"
-                        source={{
-                            uri:
-                                'https://abdulrahman.fleeti.com/save_file/uploads/provider/user/5bf637c8_60262ff8dbde39.10627959.jpg'
-                        }}
-                    />
-                </View>
-
-                <Label font={14} style={{ color: "#FFFFFF", marginTop: 8, }}>
-                    Penny N. Doamin
-                </Label>
-                <Label primary font={14} bold style={{ color: "#FFFFFF",marginTop: 8 }}>
-                    Senior Product Analyst
-                    <Label primary font={14} style={{ color: "#e2acc7" }}>
-                        {" "}at{" "}
-                    </Label>
-                    MicroSoft
-                </Label>
-
-                <View style={styles.flatListHeader}>
-                    <Text style={[styles.text,{color:"#ffff00"}]}>
-                       Play Games
-                    </Text>
-                    <Text style={styles.text}>
-                        About
-                    </Text>
-                    <Text style={styles.text}>
-                      Friends
-                    </Text>
-                </View>
-                <Label notAlign style={{ top: 5, color: "#FFFFFF", marginTop: 8, fontSize: 16 }}>
-             Played Games
-            </Label>
-            <FlatList
-              data={data}
-
-              // horizontal={true}
-              ItemSeparatorComponent={() => {
-                return (
-                  <View
-                    style={{
-                      marginTop:20,
-                      height: 1,
-                      width: "100%",
-                      backgroundColor: "#994e7c",
-                    }}
-                  />
-                );
-              }
-
-
-
-              }
-              renderItem={
-                ({ item, index }) => {
-                  return (
-                    <TriviaCard
-                    onPress={()=>navigation.navigate("LastGameWinnerProfile")} 
-                    />
-                  )
-                }
-              }
+  return (
+    <>
+      <Background height={1} />
+      <View style={{ height: 20 }} />
+      <Header back={true} />
+      <View style={styles.aView}>
+        <View style={styles.avatarView}>
+        <ProfilePicture
+              picture={userInfo?.profile_image}
+              id={userInfo?.id}
+              name={(userInfo?.first_name.slice(0, 1) + userInfo?.last_name.slice(0, 1))}
+              style={styles.avatarView}
+              font = {28}
             />
-            </View>
-        </>
-    );
+        </View>
+
+        <Label font={14} style={{ color: "#FFFFFF", marginTop: 8 }}>
+          {userInfo?.first_name} {userInfo?.last_name} 
+        </Label>
+        <Label
+          primary
+          font={14}
+          bold
+          style={{ color: "#FFFFFF", marginTop: 8 }}
+        >
+          {userInfo?.designation || "Senior Product Analyst"}
+          <Label primary font={14} style={{ color: "#e2acc7" }}>
+            {" "}
+            at{" "}
+          </Label>
+         {userInfo?.company_name || "MicroSoft"}
+        </Label>
+
+        <View style={styles.flatListHeader}>
+          <Text style={[styles.text, { color: "#ffff00" }]}>Play Games</Text>
+          <Text style={styles.text}>About</Text>
+          <Text style={styles.text}>Friends</Text>
+        </View>
+        {userData?.length>0 && (
+          <Label
+            notAlign
+            style={{ top: 5, color: "#FFFFFF", marginTop: 8, fontSize: 16 }}
+          >
+            Played Games
+          </Label>
+        )}
+        <FlatList
+          data={userData}
+          contentContainerStyle={{
+            paddingBottom: height * 0.48,
+          }}
+          ListEmptyComponent={
+            <NotFound
+              text="Games"
+              desc="You don't have win any Games yet "
+              ConModal
+            />
+          }
+          ItemSeparatorComponent={() => {
+            return (
+              <View
+                style={{
+                  marginTop: 20,
+                  height: 1,
+                  width: "100%",
+                  backgroundColor: "#994e7c",
+                }}
+              />
+            );
+          }}
+          renderItem={({ item, index }) => {
+            return (
+              <TriviaCard
+                userInfo = {userInfo}
+                userData = {item}
+                onPress={() => navigation.navigate("LastGameWinnerProfile")}
+              />
+            );
+          }}
+        />
+      </View>
+</>
+        
+  );
 };
 
 const styles = StyleSheet.create({
-    mainView: {
-        height: 800,
-        width: width,
-        alignItems: 'center',
-    },
-    newGameView: {
-        marginTop: 10,
-        width: width - 25,
-        height: height - 600,
-        justifyContent: 'center',
-        borderRadius: 20
-    },
-    btnView: {
-        marginTop: 10,
-        backgroundColor: '#ffffff',
-        width: width - 200,
-        height: height - 665,
-        justifyContent: 'center',
-        borderRadius: 30
-    },
+  mainView: {
+    height: 800,
+    width: width,
+    alignItems: "center",
+  },
+  newGameView: {
+    marginTop: 10,
+    width: width - 25,
+    height: height - 600,
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  btnView: {
+    marginTop: 10,
+    backgroundColor: "#ffffff",
+    width: width - 200,
+    height: height - 665,
+    justifyContent: "center",
+    borderRadius: 30,
+  },
 
+  aView: {
+    alignItems: "center",
+    width: widthPercentageToDP("100%"),
+    marginTop: 20,
+  },
+  flatListHeader: {
+    marginTop: heightConverter(20),
+    width: widthPercentageToDP("100%"),
+    backgroundColor: "rgba(0,0,0,0.4)",
+    height: heightConverter(65),
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  avatarView: {
+    //position: 'absolute',
 
-
-    aView: {
-        alignItems: 'center',
-        width: widthPercentageToDP("100%"),
-        marginTop: 20,
+    width: widthConverter(130),
+    height: widthConverter(130),
+    borderRadius: heightConverter(130),
+    borderWidth: 3,
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#ffffff",
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 7,
     },
-    flatListHeader: {
-        marginTop: heightConverter(20),
-        width: widthPercentageToDP("100%"),
-        backgroundColor: 'rgba(0,0,0,0.4)',
-        height: heightConverter(65),
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'space-around'
-    },
-    avatarView: {
-        //position: 'absolute',
-
-        width: widthConverter(130),
-        height: widthConverter(130),
-        borderRadius: heightConverter(130),
-        borderWidth: 3,
-        alignItems: "center",
-        justifyContent: "center",
-        borderColor: "#ffffff",
-        elevation: 5,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 7,
-        },
-        shadowOpacity: 0.43,
-        shadowRadius: 9.51,
-    },
-    text: {
-        fontFamily: "Axiforma-Regular",
-        color: '#ffffff',
-        fontSize:16
-    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+  },
+  text: {
+    fontFamily: "Axiforma-Regular",
+    color: "#ffffff",
+    fontSize: 16,
+  },
 });
-
-
 
 export default LastGame;
