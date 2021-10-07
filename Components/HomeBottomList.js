a957138d659674f3564ba54f0250531df70f5d62
mahesh-kumar26 <67702853+MaheshMedibookr@users.noreply.github.com>
2021-10-07 14:19:18 +0500

    past winner api integrated on landing page

--- Components/HomeBottomList.js
+++ Components/HomeBottomList.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Image, StyleSheet, Dimensions, TouchableOpacity, FlatList, View } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  FlatList,
+  View,
+} from "react-native";
 import Label from "../Components/Label";
 import { Images } from "../Constants/Index";
 import { useNavigation } from "@react-navigation/native";
@@ -7,56 +14,103 @@ import { connect } from "react-redux";
 import EncryptedStorage from "react-native-encrypted-storage";
 import Config from "react-native-config";
 import axios from "axios";
+import LoaderImage from "./LoaderImage";
+import { Colors } from "../Constants/Index";
 const { width, height } = Dimensions.get("window");
 
 function ClosingSoon({ item }) {
-
+  const ImgUrl = `${Config.PRODUCT_IMG}/${item?.product_id}/${item?.product_image}`;
+  const ProfileUrl = `${Config.Profile_URL}/${item?.user?.profile_image}`;
+  console.log(ProfileUrl);
   return (
-    <View style={{
-      width: width * 0.7,
-      height: height * 0.2,
-      backgroundColor: '#F4EDEF',
-      marginLeft: 10,
-      borderRadius: 10,
-      padding: 10,
-      justifyContent: 'center'
-    }}>
+    <View
+      style={{
+        width: width * 0.7,
+        height: height * 0.2,
+        backgroundColor: "#F4EDEF",
+        marginLeft: 10,
+        borderRadius: 10,
+        padding: 10,
+        justifyContent: "center",
+      }}
+    >
+      <View style={{ flexDirection: "row" }}>
+        {item.product_image || item.profile_image=== null ? (
+          <LoaderImage
+            source={{
+              uri: ImgUrl.replace("http://", "https://"),
+            }}
+            style={{
+              width: 100,
+              height: 120,
+            }}
+            resizeMode="cover"
+          />
+        ) : item?.user?.profile_image ? (
+          <LoaderImage
+            source={{
+              uri: ProfileUrl.replace("http://", "https://"),
+            }}
+            style={{
+              width: 100,
+              height: 120,
+            }}
+            resizeMode="cover"
+          />
+        ) : (
+          <View style={[styles.ProfileView, styles.ProfileBG]}>
+            <Label
+              adjustsFontSizeToFit={true}
+              font={28}
+              bold
+              style={{ textTransform: "uppercase" }}
+            >
+              {item?.user?.first_name.slice(0, 1) +
+                item?.user?.last_name.slice(0, 1)}
+            </Label>
+          </View>
+        )}
 
-      <View style={{ flexDirection: 'row', }}>
-        <Image
-          style={{
-            width: 60,
-            height: 100,
-          }}
-          resizeMode={"contain"}
-          source={{uri:item?.product_image}}
-
-        />
-        <View style={{ marginLeft: width * 0.14, }}>
+        <View style={{ marginLeft: 20 }}>
           <Label notAlign primary font={12} bold style={{ color: "#E7003F" }}>
             Congratulations
           </Label>
-          <Label notAlign bold font={12} dark style={{ color: "#000000", width: width * 0.3, }}>
-           {item.winnerfull_name || (item?.user?.first_name + " " + item?.user?.last_name)}
+          <Label
+            notAlign
+            bold
+            font={12}
+            dark
+            style={{ color: "#000000", width: width * 0.3 }}
+          >
+            {item.winnerfull_name ||
+              item?.user?.first_name + " " + item?.user?.last_name}
           </Label>
-          <Label notAlign primary font={12} dark style={{ color: "#000000", width: width * 0.3, }}>
+          <Label
+            notAlign
+            primary
+            font={12}
+            dark
+            style={{ color: "#000000", width: width * 0.3 }}
+          >
             on winning
           </Label>
-          <Label notAlign bold font={12} dark style={{ color: "#000000", width: width * 0.3, }}>
-            {item.product_title}
+          <Label
+            notAlign
+            bold
+            font={12}
+            dark
+            style={{ color: "#000000", width: width * 0.3 }}
+          >
+            {item.product_title || (+item.price).toLocaleString()}
           </Label>
         </View>
       </View>
-
     </View>
   );
 }
 const HomeBottomList = (props) => {
-
   const navigation = useNavigation();
   const { data } = props;
-
-
 
   return (
     <>
@@ -65,21 +119,19 @@ const HomeBottomList = (props) => {
       </Label>
       <FlatList
         horizontal={true}
-        style={{ marginLeft: 1, minHeight: 50, }}
-        contentContainerStyle={{ alignSelf: "flex-start",paddingRight: width * 0.03 }}
+        style={{ marginLeft: 1, minHeight: 50 }}
+        contentContainerStyle={{
+          alignSelf: "flex-start",
+          paddingRight: width * 0.03,
+        }}
         showsVerticalScrollIndicator={false}
         showsHorizontalScrollIndicator={false}
         data={data}
         renderItem={({ item }) => (
-          <ClosingSoon
-            props={props}
-            index={item.index}
-            item={item}
-
-          />
+          <ClosingSoon props={props} index={item.index} item={item} />
         )}
         keyExtractor={(item) => item.id}
-      //   ListEmptyComponent={this.RenderEmptyContainerOnGoing()}
+        //   ListEmptyComponent={this.RenderEmptyContainerOnGoing()}
       />
     </>
   );
@@ -92,7 +144,6 @@ const styles = StyleSheet.create({
     marginTop: height * 0.015,
     resizeMode: "stretch",
     alignSelf: "center",
-
   },
   LinerGradientProgrees: {
     alignItems: "center",
@@ -108,7 +159,6 @@ const styles = StyleSheet.create({
     position: "absolute",
     backgroundColor: "#EADFE3",
     borderRadius: 9,
-
   },
   containerprogressBar: {
     width: 100,
@@ -118,7 +168,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     height: 3,
     marginLeft: 2,
-
   },
   mainView: {
     height: height - 250,
@@ -128,8 +177,18 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 20,
     borderTopLeftRadius: 0,
     borderTopRightRadius: 0,
-    overflow: 'hidden',
-  }
+    overflow: "hidden",
+  },
+  ProfileView: {
+    width: 100,
+    height: 120,
+    overflow: "hidden",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  ProfileBG: {
+    backgroundColor: Colors.PROFILE_BG,
+  },
 });
 const mapStateToProps = (state) => {
   const { Bell } = state;
