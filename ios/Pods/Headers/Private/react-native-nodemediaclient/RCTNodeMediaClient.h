b78790ae0c4da5c7babe828843f185aeb18f3f51
waqarhussain19 <waqar@almostfullstack.com>
2021-12-09 15:42:14 +0400

    clean

--- ios/Pods/Headers/Private/react-native-nodemediaclient/RCTNodeMediaClient.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-nodemediaclient/ios/RCTNodeMediaClient/RCTNodeMediaClient.h
--- ios/Pods/Target Support Files/Pods-rewardz_trivia/Pods-rewardz_trivia-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_rewardz_trivia : NSObject
-@end
-@implementation PodsDummy_Pods_rewardz_trivia
-@end
